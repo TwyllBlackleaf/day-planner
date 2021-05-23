6b394a59cef37cbb2a5261feb3aa72c60c5a78dd
Twyll <twyllblackleaf@gmail.com>
2021-05-22 22:33:51 -0400

    click function for save button

--- javascript.js
+++ javascript.js
@@ -1,7 +1,18 @@
+var timesArray = ["9-am", "10-am", "11-am",
+    "12-pm", "1-pm", "2-pm", "3-pm", "4-pm", "5-pm"]
+
 var updateTime = function() {
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     console.log(moment());
 }
+
+// Click to save text in corresponding text area
+$(".fa-lock").on("click", function() {
+    var textId = $(this).attr("id").replace("btn-", "#");
+    console.log(textId);
+    console.log($(textId).val());
+    console.log("save");
+});
 
 updateTime();
 setInterval(updateTime, 1000 * 60);
